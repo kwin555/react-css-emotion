579e2ec02464866d0462979a31e91dd0256ab51c
Kevin T Nguyen <knguyen03@pros.com>
2020-01-23 11:59:26 -0600

    emotion

---
+++ src/NavBar.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Link } from "@reach/router";
+import { css, keyframes } from "@emotion/core";
+import colors from "./colors";
+
+const spin = keyframes `
+    to  {
+        transform: rotate(360deg);
+    }
+`;
+
+const NavBar = () => {
+    const [padding] = useState(15);
+    const [time, setTime] = useState(1);
+    const handleDogClick = () => {
+        setTime(time * .5);
+    };
+  return (
+        <header
+            css={css`
+            background-color: ${colors.dark};
+            padding: ${padding}px;
+            `}
+        >
+        <Link to="/">Adopt Me!</Link>
+        <span
+            onClick={() => handleDogClick}
+            css={css `
+                font-size:60px;
+                
+                &:hover {
+                    text-decoration: underline;
+                    display: inline-block;
+                    animation: ${time}s ${spin} linear infinite reverse;
+                }
+            `}
+         role="img" aria-label="logo">
+        🐕
+        </span>
+    </header>
+    )
+};
+
+export default NavBar;
